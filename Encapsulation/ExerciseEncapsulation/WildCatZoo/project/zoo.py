dd333e1116dc6a534c132bcda1900135e9c6c9d8
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2022-08-05 19:14:28 +0300

    day.commit

---
+++ Encapsulation/ExerciseEncapsulation/WildCatZoo/project/zoo.py
@@ -0,0 +1,35 @@
+class Zoo:
+    def __init__(self, name, budget, animal_capacity, workers_capacity):
+        self.name = name
+        self.__budget = budget
+        self.__animal_capacity = animal_capacity
+        self.__workers_capacity = workers_capacity
+        self.animals = []
+        self.workers = []
+
+    def add_animal(self, animal, price):
+        pass
+
+    def hire_worker(self, worker):
+        pass
+
+    def fire_workers(self):
+        pass
+
+    def pay_workers(self):
+        pass
+
+    def tend_animals(self):
+        pass
+
+    def profit(self):
+        pass
+
+    def animal_status(self):
+        pass
+
+    def workers_status(self):
+        pass
+
+
+
--- Encapsulation/LabEncapsulation/email_validator.py
+++ Encapsulation/LabEncapsulation/email_validator.py
@@ -1,17 +1,28 @@
-class EmailCalidator:
+class EmailValidator:
     def __init__(self, min_length, mails, domains):
         self.min_length = min_length
         self.mails = mails
         self.domains = domains
 
     def __is_name_valid(self, name):
-        name = self.email.split('@')[0]
+        return  len(name) >= self.min_length
 
     def __is_mail_valid(self, mail):
-        pass
+        return mail in self.mails
 
     def __is_domain_valid(self, domain):
-        pass
+        return domain in self.domains
 
     def validate(self, email):
-        self.email = email
+        user = email.split('@')[0]
+        mail, domain = email.split('@')[1].split('.')
+        return self.__is_name_valid(user) and self.__is_mail_valid(mail) and self.__is_domain_valid(domain)
+
+
+mails = ["gmail", "softuni"]
+domains = ["com", "bg"]
+email_validator = EmailValidator(6, mails, domains)
+print(email_validator.validate("pe77er@gmail.com"))
+print(email_validator.validate("georgios@gmail.net"))
+print(email_validator.validate("stamatito@abv.net"))
+print(email_validator.validate("abv@softuni.bg"))
