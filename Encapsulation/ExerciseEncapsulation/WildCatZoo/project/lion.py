42964f15268e02e449822e7fab13189960581efb
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2022-08-07 11:34:56 +0300

    pizza

--- Encapsulation/ExerciseEncapsulation/WildCatZoo/project/lion.py
+++ Encapsulation/ExerciseEncapsulation/WildCatZoo/project/lion.py
@@ -2,6 +2,5 @@ from project.animal import Animal
 
 
 class Lion(Animal):
-    def __init__(self,name, gender, age, money_for_care):
-        super().__init__(name, gender, age, money_for_care)
-        self.money_for_care = 50
+    def __init__(self,name, gender, age):
+        super().__init__(name, gender, age, money_for_care=50)
--- Encapsulation/ExerciseEncapsulation/WildCatZoo/project/zoo.py
+++ Encapsulation/ExerciseEncapsulation/WildCatZoo/project/zoo.py
@@ -1,3 +1,13 @@
+from project.animal import Animal
+from project.caretaker import Caretaker
+from project.cheetah import Cheetah
+from project.keeper import Keeper
+from project.lion import Lion
+from project.tiger import Tiger
+from project.vet import Vet
+from project.worker import Worker
+
+
 class Zoo:
     def __init__(self, name, budget, animal_capacity, workers_capacity):
         self.name = name
@@ -7,29 +17,63 @@ class Zoo:
         self.animals = []
         self.workers = []
 
-    def add_animal(self, animal, price):
-        pass
+    def add_animal(self, animal: Animal, price):
+        if len(self.animals) == self.__animal_capacity:
+            return "Not enough space for animal"
+        if price > self.__budget:
+            return "Not enough budget"
+        self.animals.append(animal)
+        self.__budget -= price
+        return f"{animal.name} the {animal.__class__.__name__} added to the zoo"
 
-    def hire_worker(self, worker):
-        pass
+    def hire_worker(self, worker: Worker):
+        if len(self.workers) == self.__workers_capacity:
+            return f"Not enough space for worker"
+        self.workers.append(worker)
+        return f"{worker.name} the {worker.__class__.__name__} hired successfully"
 
-    def fire_workers(self):
-        pass
+    def fire_worker(self, worker_name):
+        for worker in self.workers:
+            if worker_name == worker.name:
+                self.workers.remove(worker)
+                return f"{worker_name} fired successfully"
+        return f"There is no {worker_name} in the zoo"
 
     def pay_workers(self):
-        pass
+        total_salary = sum(wr.salary for wr in self.workers)
+        if total_salary > self.__budget:
+            return "You have no budget to pay your workers. They are unhappy"
+        self.__budget -= total_salary
+        return f"You payed your workers. They are happy. Budget left: {self.__budget}"
 
     def tend_animals(self):
-        pass
+        total_money_for_care = sum(mn.money_for_care for mn in self.animals)
+        if total_money_for_care > self.__budget:
+            return "You have no budget to tend the animals. They are unhappy."
+        self.__budget -= total_money_for_care
+        return f"You tended all the animals. They are happy. Budget left: {self.__budget}"
 
-    def profit(self):
-        pass
+    def profit(self, amount):
+        self.__budget += amount
 
-    def animal_status(self):
-        pass
+    def animals_status(self):
+        result = f"You have {len(self.animals)} animals\n"
+        lions = [repr(an) for an in self.animals if isinstance(an, Lion)]
+        result += f"----- {len(lions)} Lions:\n" + '\n'.join(lions) + '\n'
+
+        tigers = [repr(an) for an in self.animals if isinstance(an, Tiger)]
+        result += f"----- {len(tigers)} Tigers:\n" + '\n'.join(tigers) + '\n'
+        cheetahs = [repr(an) for an in self.animals if isinstance(an, Cheetah)]
+        result += f"----- {len(cheetahs)} Cheetahs:\n" + '\n'.join(cheetahs)
+        return result
 
     def workers_status(self):
-        pass
-
-
+        result = f"You have {len(self.workers)} workers\n"
+        keepers = [repr(wr) for wr in self.workers if isinstance(wr, Keeper)]
+        result += f"----- {len(keepers)} Keepers:\n" + '\n'.join(keepers) + '\n'
+        caretakers = [repr(wr) for wr in self.workers if isinstance(wr, Caretaker)]
+        result += f"----- {len(caretakers)} Caretakers:\n" + '\n'.join(caretakers) + '\n'
+        vets = [repr(wr) for wr in self.workers if isinstance(wr, Vet)]
+        result += f"----- {len(vets)} Vets:\n" + '\n'.join(vets)
+        return result
 
