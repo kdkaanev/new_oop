64e47ded4faf53f154228caf566480a14e8c7625
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2022-08-10 19:07:48 +0300

    final version

--- StaticAndClassMethods/ExerClassAndStaticMethods/photo_album.py
+++ StaticAndClassMethods/ExerClassAndStaticMethods/photo_album.py
@@ -1,25 +1,51 @@
-class PhotoAlbum:
-    def __init__(self, pages):
-        self.pages = pages
-        self.photos = [ for  in range(self.pages) ]
+from math import ceil
 
+
+class PhotoAlbum:
+    PHOTOS_PER_PAGE = 4
+
+    def __init__(self, pages):
+
+        self.pages = pages
+        self.photos = self.photo_album()
+
+    def photo_album(self):
+        album = []
+        for _ in range(self.pages):
+            album.append([] * PhotoAlbum.PHOTOS_PER_PAGE)
+        return album
 
     @classmethod
     def from_photos_count(cls, photos_count):
-        return cls(photos_count / 4)
+        return cls(ceil(photos_count / PhotoAlbum.PHOTOS_PER_PAGE))
 
     def add_photo(self, label):
-        for r in  range(len(self.photos)):
-            for i in range(len(self.photos)):
-                if  self.photos[r][i] == '':
-                    self.photos[r][i] = label
-                    return f"{label} photo added successfully on page {r +1} slot {i + 1}"
-            break
-        return "No more free slots"        
-
-
+        for row, page in enumerate(self.photos):
+            if len(page) < PhotoAlbum.PHOTOS_PER_PAGE:
+                page.append(label)
+                return f"{label} photo added successfully on page {row + 1} slot {len(page)}"
+        return "No more free slots"
 
     def display(self):
-        pass
+        delimiter = '-' * 11
+        result = delimiter +'\n'
+        for page in self.photos:
+            page_str = " ".join(['[]' for photo in page])
+            result += page_str + '\n' + delimiter + '\n'
+        return result.strip()
 
-'
+
+
+
+album = PhotoAlbum(2)
+
+print(album.add_photo("baby"))
+print(album.add_photo("first grade"))
+print(album.add_photo("eight grade"))
+print(album.add_photo("party with friends"))
+print(album.photos)
+print(album.add_photo("prom"))
+print(album.add_photo("wedding"))
+
+print(album.display())
+
