940ddd2703bc73b0aa7888cd0a9def3f548d62d4
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2022-08-15 18:55:51 +0300

    0.5

--- StaticAndClassMethods/ExerClassAndStaticMethods/DocumentManagement/project/storage.py
+++ StaticAndClassMethods/ExerClassAndStaticMethods/DocumentManagement/project/storage.py
@@ -9,21 +9,56 @@ class Storage:
         self.topics = []
         self.documents = []
 
-    def add_category(self, category:Category):
-        pass
+    def add_category(self, category: Category):
+        if category not in self.categories:
+            self.categories.append(category)
 
-    def add_topic(self, topic:Topic):
-        pass
+    def add_topic(self, topic: Topic):
+        if topic not in self.topics:
+            self.topics.append(topic)
 
-    def add_document(self, document:Document):
-        pass
+    def add_document(self, document: Document):
+        if document not in self.documents:
+            self.documents.append(document)
 
+    def edit_category(self, category_id: int, new_name: str):
+        category = self.__search_by_id(self.categories, category_id)
+        if category in self.categories:
+            category.name = new_name
 
-
-    def edit_category(self, category_id: int, new_name:str):
-        pass
-
+    def edit_topic(self, topic_id: int, new_topic: str, new_storage_folder: str):
+        topic = self.__search_by_id(self.topics,topic_id)
+        if topic in self.topics:
+            topic.topic  = new_topic
+            topic.storage_folder = new_storage_folder
 
     def edit_document(self, document_id: int, new_file_name: str):
-        pass
+        document = self.__search_by_id(self.documents, document_id)
+        if document in self.documents:
+            document.file_name = new_file_name
 
+    def delete_category(self, category_id):
+        category = self.__search_by_id(self.categories, category_id)
+        self.categories.remove(category)
+
+    def delete_topic(self, topic_id):
+        topic = self.__search_by_id(self.topics, topic_id)
+        self.topics.remove(topic)
+
+    def delete_document(self, document_id):
+        document = self.__search_by_id(self.documents, document_id)
+        self.documents.remove(document)
+
+    def get_document(self, document_id):
+        document = self.__search_by_id(self.documents, document_id)
+        return document
+
+    def __repr__(self):
+        for dok in self.documents:
+            return str(dok)
+
+
+    def __search_by_id(self, colections, obj_id):
+        for obj in colections:
+            if obj.id == obj_id:
+                return obj
