36d2fdfd6c2bcf4278892a040ced075d1a27587e
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2022-08-27 08:59:19 +0300

    final

--- PolymorphismAndAbstraction/Ex/groups.py
+++ PolymorphismAndAbstraction/Ex/groups.py
@@ -8,11 +8,43 @@ class Person:
         return f'{self.name} {self.surname}'
 
     def __add__(self, other):
-        new_person = self.name + other.surname
-        return new_person
+        return Person(self.name, other.surname)
 
 
-p = Person('Pesho', 'Peshev')
-p1 = Person('Ivan', 'Ivanov')
-print(p + p1)
-print(p)
+class Group:
+    def __init__(self, name, people):
+        self.name = name
+        self.people = people
+
+    def __len__(self):
+        return len(self.people)
+
+    def __add__(self, other):
+        group_name = f'{self.name} {other.name}'
+        members = self.people + other.people
+        return Group(group_name, members)
+
+    def __str__(self):
+        str_name = ', '.join([str(p) for p in self.people])
+        return f'Group {self.name} with members {str_name}'
+
+    def __getitem__(self, index):
+        return f"Person {index}: {str(self.people[index])}"
+
+
+p0 = Person('Aliko', 'Dangote')
+p1 = Person('Bill', 'Gates')
+p2 = Person('Warren', 'Buffet')
+p3 = Person('Elon', 'Musk')
+p4 = p2 + p3
+
+first_group = Group('__VIP__', [p0, p1, p2])
+second_group = Group('Special', [p3, p4])
+third_group = first_group + second_group
+
+print(len(first_group))
+print(second_group)
+print(third_group[0])
+
+for person in third_group:
+    print(person)
