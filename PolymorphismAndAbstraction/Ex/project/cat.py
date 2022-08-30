3627f54ac200a07112f770968dacb9e0029614b8
kdkaanev <kdkaanev@github.com>
2022-08-30 08:15:49 +0300

    new commit from prisoe

--- PolymorphismAndAbstraction/Ex/project/cat.py
+++ PolymorphismAndAbstraction/Ex/project/cat.py
@@ -1,4 +1,4 @@
-from project.animal import Animal
+from animal import Animal
 
 class Cat(Animal):
     def __init__(self, name, age, gender):
--- PolymorphismAndAbstraction/Ex/project/kitten.py
+++ PolymorphismAndAbstraction/Ex/project/kitten.py
@@ -1,9 +1,9 @@
-from project.cat import Cat
+from cat import Cat
 
 class Kitten(Cat):
     def __init__(self, name, age):
         super().__init__(name, age,gender='Female')
 
-
-k = Kitten('a',2, 'ww')
-print(k)
+    
+    def  make_sownd(self):
+        return 'Meow'
