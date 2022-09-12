9164edfb6696f48d4dac427a7d2494dfdcb62040
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2022-09-12 22:30:33 +0200

    final to sday

--- SOLID/SolidExe/exercise-resources/shapes.py
+++ SOLID/SolidExe/exercise-resources/shapes.py
@@ -1,13 +1,33 @@
-class Rectangle:
+import abc
+
+
+class Shapes(abc.ABC):
+    @abc.abstractmethod
+    def calc_area(self):
+        pass
+
+
+class Huj(Shapes):
+    def __init__(self, big):
+        self.big = big
+
+    def calc_area(self):
+        return self.big * 3
+
+
+class Rectangle(Shapes):
 
     def __init__(self, width, height):
         self.width = width
         self.height = height
 
+    def calc_area(self):
+        return self.width * self.height
+
+
 class AreaCalculator:
 
     def __init__(self, shapes):
-
         assert isinstance(shapes, list), "`shapes` should be of type `list`."
         self.shapes = shapes
 
@@ -15,11 +35,11 @@ class AreaCalculator:
     def total_area(self):
         total = 0
         for shape in self.shapes:
-            total += shape.width * shape.height
+            total += shape.calc_area()
 
         return total
 
 
-shapes = [Rectangle(2, 3), Rectangle(1, 6)]
+shapes = [Rectangle(2, 3), Rectangle(1, 6), Huj(4)]
 calculator = AreaCalculator(shapes)
 print("The total area is: ", calculator.total_area)
