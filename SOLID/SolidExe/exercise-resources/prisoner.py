ba0691305c5d2c15795429847e621859ad649e15
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2022-09-12 22:03:37 +0200

    final

--- SOLID/SolidExe/exercise-resources/prisoner.py
+++ SOLID/SolidExe/exercise-resources/prisoner.py
@@ -32,8 +32,9 @@ print("The prisoner trying to walk to north by 10 and east by -3.")
 try:
     prisoner.walk_north(10)
     prisoner.walk_east(-3)
-except:
-    pass
+
+except Exception as error:
+    print(error)
 
 print(f"The location of the prison: {prisoner.PRISON_LOCATION}")
 print(f"The current position of the prisoner: {prisoner.position}")
