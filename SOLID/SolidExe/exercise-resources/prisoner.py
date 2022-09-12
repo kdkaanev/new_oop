043fc95378a64bc3d5019a44d010fc7cbc7ec87d
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2022-09-12 21:59:51 +0200

    prisoner final

--- SOLID/SolidExe/exercise-resources/prisoner.py
+++ SOLID/SolidExe/exercise-resources/prisoner.py
@@ -1,9 +1,16 @@
 import copy
 
+
 class Person:
 
     def __init__(self, position):
         self.position = position
+
+
+
+class FreePerson(Person):
+    def __init__(self, position):
+        super().__init__(position)
 
     def walk_north(self, dist):
         self.position[1] += dist
@@ -17,8 +24,6 @@ class Prisoner(Person):
 
     def __init__(self):
         super(Prisoner, self).__init__(copy.copy(self.PRISON_LOCATION))
-        self.is_free = False
-
 
 
 prisoner = Prisoner()
@@ -32,3 +37,4 @@ except:
 
 print(f"The location of the prison: {prisoner.PRISON_LOCATION}")
 print(f"The current position of the prisoner: {prisoner.position}")
+
