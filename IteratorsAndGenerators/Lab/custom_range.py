61038b8265b18e03249151d022bc742f4f89b240
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2022-09-16 19:14:25 +0200

    custom_range v2.0

--- IteratorsAndGenerators/Lab/custom_range.py
+++ IteratorsAndGenerators/Lab/custom_range.py
@@ -1,20 +1,28 @@
+class custom_range_iterator:
+    def __init__(self,cr):
+        self.cr = cr
+        self.next_value = self.cr.start
+
+
+    def __next__(self):
+        if self.next_value > self.cr.end:
+            raise StopIteration()
+
+        i = self.next_value
+        self.next_value += 1
+        return i
+
+
 class custom_range:
 
     def __init__(self, start, end):
         self.start = start
         self.end = end
-        self.next_value = start
 
     def __iter__(self):
-        return self
+        return custom_range_iterator(self)
 
-    def __next__(self):
-        if self.next_value <= self.end:
-            i = self.next_value
-            self.next_value += 1
-            return i
-        else:
-            raise StopIteration()
+
 
 one_to_ten = custom_range(1, 10)
 for num in one_to_ten:
