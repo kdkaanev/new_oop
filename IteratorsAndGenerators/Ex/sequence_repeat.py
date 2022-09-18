46a2163ad760841b6ad691c8a0ad4db1e25f5ce0
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2022-09-18 11:34:32 +0200

    seq_rep2.0 final

--- IteratorsAndGenerators/Ex/sequence_repeat.py
+++ IteratorsAndGenerators/Ex/sequence_repeat.py
@@ -2,7 +2,6 @@ class sequence_repeat:
     def __init__(self, sequence, number):
         self.sequence = sequence
         self.number = number
-        self.text = ''
         self.idx = 0
         self.counter = 0
 
@@ -10,19 +9,16 @@ class sequence_repeat:
         return self
 
     def __next__(self):
-
         while self.counter < self.number:
-
-                self.text = self.sequence[self.idx]
-                self.idx += 1
-                if self.idx == len(self.sequence):
-                    self.idx = 0
-                self.counter += 1
-                return self.text
+            self.text = self.sequence[self.idx]
+            self.idx += 1
+            if self.idx == len(self.sequence):
+                self.idx = 0
+            self.counter += 1
+            return self.text
         raise StopIteration
 
 
 result = sequence_repeat('I Love Python', 3)
 for item in result:
-    print(item, end ='')
-
+    print(item, end='')
