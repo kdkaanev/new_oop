b56265d2febf46bb9d1e796ba04a87b41a9a325e
kdkaanev <kdkaanev@github.com>
2022-10-06 11:51:50 +0300

    from prisoe

---
+++ Decorators/Ex/execution_time.py
@@ -0,0 +1,23 @@
+import time
+
+
+def exec_time(func_ref):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        func_ref(*args)
+        end = time.time()
+        return end - start
+    return wrapper
+
+
+
+
+
+
+@exec_time
+def concatenate(strings):
+    result = ""
+    for string in strings:
+        result += string
+    return result
+print(concatenate(["a" for i in range(1000000)]))
