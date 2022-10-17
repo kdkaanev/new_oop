9e47de9b37e7e31f00f15b904627285e189e3086
kdkaanev <kdkaanev@github.com>
2022-10-17 08:59:56 +0300

    car_test first rev

---
+++ Testing/Lab/Lab-Resources/CarManager/test_car.py
@@ -0,0 +1,56 @@
+from unittest import TestCase
+from car_manager import Car
+
+
+class TestCar(TestCase):
+    MAKE = 'Honda'
+    MODEL = 'Jazz'
+    FUEL_CONSUMPTON = 6.5
+    FUEL_CAPACITY = 50
+    FUEL_AMOUNT = 0
+
+    def setUp(self) -> None:
+        car = Car(self.MAKE, self.MODEL, self.FUEL_CONSUMPTON, self.FUEL_CAPACITY)
+
+    def test_init(self):
+        pass
+
+    def test_make__when_is_null_or_empty__espected_raise(self):
+        pass
+
+
+    def test_make__when_is_not_null_or_empty__espected_value(self):
+        pass
+
+    def test_model__when_is_null_or_empty__espected_raise(self):
+        pass
+
+    def test_model__when_is_not_null_or_empty__espected_value(self):
+        pass
+
+    def test_fuel_cpacity__when_value_zero_or_negative__espected_raise(self):
+        pass
+
+    def test_fuel_cpacity__when_value_is_positive(self):
+        pass
+
+    def test_fuel_amount__when_value_is_negative__espected_raise(self):
+        pass
+
+    def test_fuel_amount__when_value_is_positive(self):
+        pass
+
+    def test_refuel__when_value_zero_or_negative__espected_raise(self):
+        pass
+
+    def test_refuel__when_value_is_positive(self):
+        pass
+
+    def test_refuel__when_value_is_equal_to_fuel_capacity(self):
+        pass
+
+    def test_distance__when_fuel_not_enough__espected_raise(self):
+        pass
+
+    def test_distance__espected_fuel_decrace(self):
+        pass
--- Testing/Lab/Lab-Resources/List/test_extended_list.py
+++ Testing/Lab/Lab-Resources/List/test_extended_list.py
@@ -8,24 +8,36 @@ class TestIntegerList(TestCase):
     DATA = []
     def setUp(self) -> None:
         self.integer = IntegerList()
+        
     def test_init__input_args_espected_int(self):
         self.assertEqual(self.DATA, self.integer.__data)
 
     def test_add__element_not_integer__espected_raise(self):
         integer = IntegerList('huj')
-        with self.assertRaises(ValueError, "Element is not Integer") as ex:
+        with self.assertRaises(ValueError) as ex:
             integer.add()
-            self.assertIsNotNone(ex)
+            self.assertEqual( "Element is not Integer", ex)
 
 
     def test_remove_index__index_out_of_range__espected_raise(self):
-        
-        with self.assertRaises(IndexError, "Index is out of range") as ex:
-            pass
-    def test_get__index_out_of_range__espected_raise(self):
-        pass
+        self.DATA = []
+        with self.assertRaises(IndexError) as ex:
+            self.integer.remove_index()
+            self.assertEqual("Index is out of range", ex)
 
-    def test_insert__index_out_of_range_and_element_not_integer__espected_raise(self):
+    def test_get__index_out_of_range__espected_raise(self):
+        self.DATA = []
+        with self.assertRaises(IndexError) as ex:
+            self.integer.get()
+            self.assertEqual("Index is out of range", ex)
+            
+
+    def test_insert__index_out_of_range__espected_raise(self):
+        with self.assertRaises(IndexError) as ex:
+            self.integer.test()
+            self.assertEqual("Index is out of range", ex)
+
+    def test_insert__element_not_integer__espected_raise(self):
         pass
 
     def test_get_biggest__espected_element_0_is_biggest(self):
