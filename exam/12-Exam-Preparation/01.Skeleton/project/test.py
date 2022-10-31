df06b3ec3ee374b3a5f876e6b0228a134b9e1bd1
kdkaanev <kdkaanev@github.com>
2022-10-31 16:18:47 +0200

    changes

--- exam/12-Exam-Preparation/01.Skeleton/project/test.py
+++ exam/12-Exam-Preparation/01.Skeleton/project/test.py
@@ -1,7 +1,9 @@
+from controller import Controller
 from player import Player
 
 
 player = Player('putjo', 25, 17)
-player2 = Player('putjo', 23, 34)
-print(player)
+player2 = Player('putjo1', 23, 34)
+controller = Controller()
+print(controller.add_player(player, player2))
 
